e8c582112603d8e1be8d17fcf0967bedd050f6d5
Niels Penneman <niels.penneman@elis.ugent.be>
2011-10-10 15:12:04 +0200

    Split interpreter for coprocessor instructions

--- src/instructionEmu/interpreter/arm/makefile.mk
+++ src/instructionEmu/interpreter/arm/makefile.mk
@@ -1,3 +1,4 @@
 HYPARM_SRCS_C-y += instructionEmu/interpreter/arm/branchInstructions.c
+HYPARM_SRCS_C-y += instructionEmu/interpreter/arm/coprocInstructions.c
 HYPARM_SRCS_C-y += instructionEmu/interpreter/arm/loadInstructions.c
 HYPARM_SRCS_C-y += instructionEmu/interpreter/arm/storeInstructions.c
---
+++ src/instructionEmu/interpreter/coprocInstructions.h
@@ -0,0 +1,28 @@
+#ifndef __INSTRUCTION_EMU__COPROC_INSTRUCTIONS_H_
+#define __INSTRUCTION_EMU__COPROC_INSTRUCTIONS_H_
+
+#include "common/types.h"
+
+#include "guestManager/guestContext.h"
+
+
+u32int armCdpInstruction(GCONTXT *context, u32int instruction);
+u32int armCdp2Instruction(GCONTXT *context, u32int instruction);
+
+u32int armLdcInstruction(GCONTXT *context, u32int instruction);
+u32int armLdc2Instruction(GCONTXT *context, u32int instruction);
+
+u32int armMcrInstruction(GCONTXT *context, u32int instruction);
+u32int armMcr2Instruction(GCONTXT *context, u32int instruction);
+u32int armMcrrInstruction(GCONTXT *context, u32int instruction);
+u32int armMcrr2Instruction(GCONTXT *context, u32int instruction);
+
+u32int armMrcInstruction(GCONTXT *context, u32int instruction);
+u32int armMrc2Instruction(GCONTXT *context, u32int instruction);
+u32int armMrrcInstruction(GCONTXT *context, u32int instruction);
+u32int armMrrc2Instruction(GCONTXT *context, u32int instruction);
+
+u32int armStcInstruction(GCONTXT *context, u32int instruction);
+u32int armStc2Instruction(GCONTXT *context, u32int instruction);
+
+#endif
