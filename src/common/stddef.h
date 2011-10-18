52b2d5d195667d8b799d86834333abda8dc0e654
Niels Penneman <niels.penneman@elis.ugent.be>
2011-10-18 15:07:51 +0200

    Add offsetof to stddef.h

--- src/common/stddef.h
+++ src/common/stddef.h
@@ -1,6 +1,8 @@
 #ifndef __COMMON__STDDEF_H__
 #define __COMMON__STDDEF_H__
 
-#define NULL  ((void *)0)
+#define NULL                    ((void *)0)
+
+#define offsetof(type, member)  __builtin_offsetof(type, member)
 
 #endif
