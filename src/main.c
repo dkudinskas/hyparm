9387fe91560c217e6e6187e493de088c9889022a
Niels Penneman <niels.penneman@elis.ugent.be>
2011-10-24 15:33:43 +0200

    Improve command line processing

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include "cli/cli.h"
 #endif
 
+#include "common/commandLine.h"
 #include "common/debug.h"
 #include "common/memFunctions.h"
 #include "common/stdarg.h"
@@ -14,10 +15,6 @@
 #include "drivers/beagle/beGPTimer.h"
 #include "drivers/beagle/beClockMan.h"
 #include "drivers/beagle/beUart.h"
-
-#ifdef CONFIG_GUEST_FREERTOS
-#include "drivers/beagle/beGPIO.h"
-#endif
 
 #ifdef CONFIG_MMC
 #include "drivers/beagle/beMMC.h"
@@ -43,16 +40,37 @@
 #include "memoryManager/frameAllocator.h"
 
 #include "vm/omap35xx/hardwareLibrary.h"
-#include "vm/omap35xx/led.h"
 
 
 #define HIDDEN_RAM_START   0x8f000000
 #define HIDDEN_RAM_SIZE    0x01000000 // 16 MB
 
+#define CL_OPTION_FAST_UART          1
+#define CL_OPTION_GUEST_OS           2
+#define CL_OPTION_GUEST_KERNEL       3
+#define CL_OPTION_GUEST_INITRD       4
+
+#define CL_VALUE_GUEST_OS_FREERTOS   "freertos"
+#define CL_VALUE_GUEST_OS_LINUX      "linux"
+
+#define RT_CONFIG_GUEST_OS_LINUX     1
+#define RT_CONFIG_GUEST_OS_FREERTOS  2
+
+
+struct runtimeConfiguration
+{
+  u32int guestOS; // ENUM ?
+  u32int guestKernelAddress;
+  u32int guestInitialRAMDiskAddress;
+};
+
+
+static void dumpRuntimeConfiguration(struct runtimeConfiguration *config);
+static void processCommandLine(struct runtimeConfiguration *config, s32int argc, char *argv[]);
+static bool stringToAddress(const char *str, u32int *address);
+
 extern void setGuestContext(GCONTXT *gContext);
 
-static void printUsage(void);
-static int parseCommandline(int argc, char *argv[], u32int *kernAddr, u32int *initrdAddr);
 
 #ifdef CONFIG_MMC
 fatfs mainFilesystem;
@@ -66,15 +84,18 @@ bool rtos;
 #endif
 
 
+static void dumpRuntimeConfiguration(struct runtimeConfiguration *config)
+{
+  DEBUG(STARTUP, "Guest OS: %#x" EOL, config->guestOS);
+  DEBUG(STARTUP, "Guest OS kernel address: %#.8x" EOL, config->guestKernelAddress);
+  DEBUG(STARTUP, "Guest OS initial RAM disk address: %#.8x" EOL, config->guestInitialRAMDiskAddress);
+}
+
 void main(s32int argc, char *argv[])
 {
-  u32int kernAddr = 0;
-  u32int initrdAddr = 0;
-  int ret = 0;
-
-#ifdef CONFIG_GUEST_FREERTOS
-  rtos = FALSE;
-#endif
+  struct runtimeConfiguration config;
+  memset(&config, 0, sizeof(struct runtimeConfiguration));
+  config.guestOS = RT_CONFIG_GUEST_OS_LINUX;
 
   /* save power: cut the clocks to the display subsystem */
   cmDisableDssClocks();
@@ -87,19 +108,23 @@ void main(s32int argc, char *argv[])
   beUartInit(1);
   beUartInit(2);
   beUartInit(3);
+
 #ifdef CONFIG_UART_FAST
   beUartStartup(3, 500000);
 #else
   beUartStartup(3, 115200);
 #endif
 
-#ifndef CONFIG_CLI
-  if ((ret = parseCommandline(argc, argv, &kernAddr, &initrdAddr)) < 0)
-  {
-    printUsage();
-    DIE_NOW(NULL, "Hypervisor startup aborted.");
-  }
-#endif /* CONFIG_CLI */
+  /*
+   * Use command line arguments passed by U-Boot to update the runtime configuration structure. The
+   * first argument is always the load address of the hypervisor; skip it.
+   */
+  processCommandLine(&config, argc - 1, argv + 1);
+  dumpRuntimeConfiguration(&config);
+
+#ifdef CONFIG_GUEST_FREERTOS
+  rtos = config.guestOS == RT_CONFIG_GUEST_OS_FREERTOS;
+#endif
 
   /* create the frametable from which we can alloc memory */
   initialiseFrameTable();
@@ -155,100 +180,105 @@ void main(s32int argc, char *argv[])
 #endif /* CONFIG_MMC */
 
   // does not return
+  switch (config.guestOS)
+  {
 #ifdef CONFIG_GUEST_FREERTOS
-  if (ret == 2)
-  {
-    DEBUG(STARTUP, "RTOS address: %x" EOL, kernAddr);
-    rtos = TRUE;
-    bootFreeRtos(context, kernAddr);
-  }
-  else
+    case RT_CONFIG_GUEST_OS_FREERTOS:
+      bootFreeRtos(context, config.guestKernelAddress);
+      break;
 #endif
-  {
-    DEBUG(STARTUP, "Kernel address: %x, Initrd address: %x" EOL, kernAddr, initrdAddr);
-    bootLinux(context, kernAddr, initrdAddr);
+    case RT_CONFIG_GUEST_OS_LINUX:
+      bootLinux(context, config.guestKernelAddress, config.guestInitialRAMDiskAddress);
+      break;
+    default:
+      printf("Error: unsupported guest OS %#x", config.guestOS);
+      break;
   }
+  DIE_NOW(context, "guest boot failed");
+
 #endif /* CONFIG_CLI */
 }
 
-static void printUsage(void)
+static void processCommandLine(struct runtimeConfiguration *config, s32int argc, char *argv[])
 {
-  printf(
-      "Loader usage:" EOL
-      "go <loaderAddr> -kernel <kernAddress> -initrd <initrdAddr>" EOL
-      "kernel: address of kernel in hexadecimal representation" EOL
-      "initrd: address of external initrd in hexadecimal representation" EOL
-#ifdef CONFIG_GUEST_FREERTOS
-      "rtos: address of rtos in hex format (0xXXXXXXXX)" EOL
-#endif
-    );
-  return;
+  bool success = TRUE;
+  struct commandLine *commandLine, *p;
+  struct commandLineOption *options = NULL;
+  options = addCommandLineOption(options, "guest", "Guest operating system type", TRUE, FALSE, CL_OPTION_GUEST_OS);
+  options = addCommandLineOption(options, "kernel", "Address of the kernel in memory", TRUE, TRUE, CL_OPTION_GUEST_KERNEL);
+  options = addCommandLineOption(options, "initrd", "Address of an initial RAM disk in memory", TRUE, FALSE, CL_OPTION_GUEST_INITRD);
+  commandLine = parseCommandLine(options, argc, argv);
+  for (p = commandLine; p; p = p->next)
+  {
+    switch (p->argumentId)
+    {
+      case CL_OPTION_GUEST_OS:
+        if (config->guestOS)
+        {
+          printf("Error: duplicate option: guest OS '%s'" EOL, p->value);
+          success = FALSE;
+        }
+        else if (strcmp(p->value, CL_VALUE_GUEST_OS_FREERTOS))
+        {
+          config->guestOS = RT_CONFIG_GUEST_OS_FREERTOS;
+        }
+        else if (strcmp(p->value, CL_VALUE_GUEST_OS_LINUX))
+        {
+          config->guestOS = RT_CONFIG_GUEST_OS_LINUX;
+        }
+        else
+        {
+          printf("Error: invalid guest OS '%s'" EOL, p->value);
+          success = FALSE;
+        }
+        break;
+      case CL_OPTION_GUEST_KERNEL:
+        if (config->guestKernelAddress)
+        {
+          printf("Error: duplicate option: kernel address '%s'" EOL, p->value);
+          success = FALSE;
+        }
+        else if (!stringToAddress(p->value, &(config->guestKernelAddress)) || !config->guestKernelAddress)
+        {
+          printf("Error: invalid kernel address '%s'" EOL, p->value);
+          success = FALSE;
+        }
+        break;
+      case CL_OPTION_GUEST_INITRD:
+        if (config->guestInitialRAMDiskAddress)
+        {
+          printf("Error: duplicate option: RAM disk address '%s'" EOL, p->value);
+          success = FALSE;
+        }
+        else if (!stringToAddress(p->value, &(config->guestInitialRAMDiskAddress)) || !config->guestInitialRAMDiskAddress)
+        {
+          printf("Error: invalid initial RAM disk address '%s'" EOL, p->value);
+          success = FALSE;
+        }
+        break;
+      default:
+        printf("Error: unrecognized option '%s'" EOL, p->value);
+        success = FALSE;
+        break;
+    }
+  }
+  freeCommandLine(commandLine);
+  if (!success)
+  {
+    printCommandLineHelp(options);
+    DIE_NOW(NULL, "bad command line");
+  }
+  freeCommandLineOptions(options);
 }
 
-static int parseCommandline(s32int argc, char *argv[], u32int *kernAddr, u32int *initrdAddr)
+static bool stringToAddress(const char *str, u32int *address)
 {
-  s32int i;
-#ifdef CONFIG_GUEST_FREERTOS
-  bool rtos;
-#endif
-
-  DEBUG(STARTUP, "parseCommandline: argc = %d" EOL, argc);
-  for (i = 0; i < argc; ++i)
+  u32int value;
+  s32int length;
+  if (sscanf(str, "%x%n", &value, &length) == 1 && (u32int)length == strlen(str))
   {
-    DEBUG(STARTUP, "parseCommandline: argv[%d] = %p" EOL, i, argv[i]);
+    *address = value;
+    return TRUE;
   }
-
-  if (argc < 3)
-  {
-    return -1;
-  }
-
-  if (strcmp("-kernel", argv[1]) == 0)
-  {
-#ifdef CONFIG_GUEST_FREERTOS
-    rtos = FALSE;
-  }
-  else if (strcmp("-rtos", argv[1]) == 0)
-  {
-    rtos = TRUE;
-#endif
-  }
-  else
-  {
-    DEBUG(STARTUP, "parseCommandline: parameter -kernel or -rtos not found" EOL);
-    return -1;
-  }
-
-  if (sscanf(argv[2], "%x", kernAddr) != 1)
-  {
-    DEBUG(STARTUP, "parseCommandline: parameter value for %s is not a valid address: '%s'" EOL, argv[1], argv[2]);
-    return -1;
-  }
-
-#ifdef CONFIG_GUEST_FREERTOS
-  if (rtos)
-  {
-    return 2;
-  }
-#endif
-
-  if (argc != 5)
-  {
-    return -1;
-  }
-
-  if (strcmp("-initrd", argv[3]) != 0)
-  {
-    DEBUG(STARTUP, "parseCommandline: parameter -initrd not found" EOL);
-    return -1;
-  }
-
-  if (sscanf(argv[4], "%x", initrdAddr) != 1)
-  {
-    DEBUG(STARTUP, "parseCommandline: parameter value for -initrd is not a valid address: '%s'" EOL, argv[4]);
-    return -1;
-  }
-
-  return 1;
+  return FALSE;
 }
-
