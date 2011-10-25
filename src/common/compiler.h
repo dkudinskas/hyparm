17743e90a3546fe9cbb8545f6ccd832597f3503d
Niels Penneman <niels.penneman@elis.ugent.be>
2011-10-25 10:13:24 +0200

    Update compiler.h
    This completes commit 0868739b017e37e02353d554791db293a29e2771.

--- src/common/compiler.h
+++ src/common/compiler.h
@@ -20,7 +20,7 @@
 #ifdef __GNUC__
 #if (__GNUC__ == 4 && __GNUC_MINOR__ >= 6) || __GNUC__ > 4
 #define COMPILER_CAN_HIDE_WARNINGS
-#define COMPILER_HAS_GCC_VECTOR
+#define COMPILER_HAS_GCC_VECTOR_TYPES
 #endif
 #endif
 
