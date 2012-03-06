e4b95d60229d41c27e7856386d19e47d844b1940
Niels Penneman <niels.penneman@elis.ugent.be>
2012-03-06 19:39:47 +0100

    Clean up (stick to conventions, add new bit operation abstraction)

--- src/instructionEmu/interpreter/t32/miscInstructions.c
+++ src/instructionEmu/interpreter/t32/miscInstructions.c
@@ -6,7 +6,8 @@
 
 #include "instructionEmu/interpreter/t32/miscInstructions.h"
 
-u32int t32MRSInstruction(GCONTXT *context, u32int instruction)
+
+u32int t32MrsInstruction(GCONTXT *context, u32int instruction)
 {
   u32int reg = (instruction & 0x0F00) >> 8;
   if (context->CPSR & PSR_USR_MODE)
@@ -43,7 +44,7 @@ u32int t32MRSInstruction(GCONTXT *context, u32int instruction)
         case PSR_USR_MODE:
         case PSR_SYS_MODE:
         default:
-          DIE_NOW(context, "mrsInstruction: cannot request spsr in user/system mode");
+          DIE_NOW(context, "cannot request spsr in user/system mode");
       }
       storeGuestGPR(reg, value, context);
     }
