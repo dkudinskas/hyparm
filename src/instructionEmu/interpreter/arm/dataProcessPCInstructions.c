e321cffb91050febec53b72bb4105c8c0725f14b
Niels Penneman <niels.penneman@elis.ugent.be>
2012-05-12 19:36:25 +0200

    Fix: MOVS translation must update condition flags in PSR

--- src/instructionEmu/interpreter/arm/dataProcessPCInstructions.c
+++ src/instructionEmu/interpreter/arm/dataProcessPCInstructions.c
@@ -7,7 +7,8 @@
 enum
 {
   RD_INDEX = 12,
-  RM_INDEX = 0
+  RM_INDEX = 0,
+  SETFLAGS_BIT = 1 << 20
 };
 
 
@@ -39,16 +40,23 @@ u32int *armMovPCInstruction(TranslationCache *tc, u32int *instructionAddr, u32in
   const u32int destinationRegister = ARM_EXTRACT_REGISTER(instruction, RD_INDEX);
   const u32int sourceRegister = ARM_EXTRACT_REGISTER(instruction, RM_INDEX);
 
-  ASSERT(destinationRegister != GPR_PC, "MOV PC must trap");
+  ASSERT(destinationRegister != GPR_PC, "Rd=PC must trap");
 
+  /*
+   * A MOV Rd,PC can be translated to only MOVT & MOVW. MOVS requires an update of the condition
+   * flags and since MOVT does not support setting flags, an extra MOVS Rd,Rd is required.
+   */
   if (sourceRegister == GPR_PC)
   {
     currBlockCopyCacheAddr = armWritePCToRegister(tc, currBlockCopyCacheAddr, ARM_EXTRACT_CONDITION_CODE(*instructionAddr), destinationRegister, pc);
+    if (!(instruction & SETFLAGS_BIT))
+    {
+      return ++currBlockCopyCacheAddr;
+    }
+    instruction = ARM_SET_REGISTER(instruction, RM_INDEX, destinationRegister);
   }
-  else
-  {
-    currBlockCopyCacheAddr = updateCodeCachePointer(tc, currBlockCopyCacheAddr);
-    *(currBlockCopyCacheAddr++) = instruction;
-  }
+
+  currBlockCopyCacheAddr = updateCodeCachePointer(tc, currBlockCopyCacheAddr);
+  *(currBlockCopyCacheAddr++) = instruction;
   return currBlockCopyCacheAddr;
 }
