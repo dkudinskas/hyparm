abb7a20f4805c92458190774d6619e3451aee718
Niels Penneman <niels.penneman@elis.ugent.be>
2012-05-12 18:00:37 +0200

    Clean up STM translation handler; add comments on how to correctly translate all STMs

--- src/instructionEmu/interpreter/arm/loadStorePCInstructions.c
+++ src/instructionEmu/interpreter/arm/loadStorePCInstructions.c
@@ -1,3 +1,4 @@
+#include "common/bit.h"
 #include "common/debug.h"
 
 #include "cpuArch/constants.h"
@@ -19,7 +20,12 @@ enum
 
   LDR_LDRB_REGISTER_FORM_BITS = 0b11 << 25,
   LDRH_LDRD_IMMEDIATE_FORM_BIT = 1 << 22,
-  LDRD_BIT = 1 << 6
+  LDRD_BIT = 1 << 6,
+
+  STM_RN_INDEX = 16,
+  STM_REGISTERS_MASK = 0xFFFF,
+  STM_REGISTERS_PC_BIT = 1 << GPR_PC,
+  STM_WRITEBACK_BIT = 1 << 21
 };
 
 
@@ -70,20 +76,63 @@ u32int *armLdrStrPCInstruction(TranslationCache *tc, u32int *instructionAddr, u3
 u32int* armStmPCInstruction(TranslationCache *tc, u32int *instructionAddr, u32int *currBlockCopyCacheAddr, u32int *blockCopyCacheStartAddress)
 {
   u32int instruction = *instructionAddr;
-  if ((instruction & 0xF0000) == 0xF0000)
-  {
-    // According to ARM ARM: source register = PC ->  UNPREDICTABLE
-    DIE_NOW(NULL, "stm PC had PC as Rn -> UNPREDICTABLE?!");
-  }
-  else
-  {
-    // Niels: FIXME
-    DIE_NOW(NULL, "BUG pC CAN BE USED");
-    //Stores multiple registers to consecutive memory locations
-    //PC is not used -> instruction is save to execute just copy it to blockCopyCache
-    currBlockCopyCacheAddr = updateCodeCachePointer(tc, currBlockCopyCacheAddr);
-    *(currBlockCopyCacheAddr++) = instruction;
 
-    return currBlockCopyCacheAddr;
+  ASSERT(ARM_EXTRACT_REGISTER(instruction, STM_RN_INDEX) != GPR_PC, "Rn=PC unpredictable");
+
+  if ((instruction & STM_REGISTERS_PC_BIT))
+  {
+    /*
+     * How to perform similar STM on all registers except PC, if any:
+     *
+     * instruction &= ~STM_REGISTERS_PC_BIT;
+     * if ((instruction & STM_REGISTERS_MASK) != 0)
+     * {
+     *   currBlockCopyCacheAddr = updateCodeCachePointer(tc, currBlockCopyCacheAddr);
+     *   *currBlockCopyCacheAddr = instruction;
+     * }
+     *
+     * We must also store PC. How all of this works together depends on STM variant (IA/DA/DB/IB)!
+     * All translated instructions inherit condition code from original.
+     *
+     * regcnt := countBitsSet(*instructionAddr & STM_REGISTERS_MASK)
+     *
+     * STMIA:
+     * perform STMIA Rn, registers without PC
+     * backup Rtemp, set to PC
+     * if W=0: STR Rtemp, [Rn, #(4*(regcnt-1))] (offset)
+     * if W=1: STR Rtemp, [Rn], #4 (post-indexed)
+     * restore Rtemp
+     *
+     * STMDA (double check this):
+     * perform SUB Rn, Rn, #4
+     * perform STMDA Rn, registers without PC
+     * backup Rtemp, set to PC
+     * if W=0: STR Rtemp, [Rn, #4]! (pre-indexed)
+     * if W=1: STR Rtemp, [Rn, #(4*regcnt)] (offset)
+     * restore Rtemp
+     * or ALTERNATIVE without SUB: replace STMDA by STMDB this will start at one word lower anyway...
+     *                             be careful with writeback, offsets must be correct!!!
+     *
+     * STMDB is similar to STMDA, but it starts writing at address= Rn-4*regcnt
+     *                            instead of Rn-4*regcnt+4 ==> 1 word LOWER in memory
+     * perform SUB Rn, Rn, #4 (required in this case)
+     * perform STMDB Rn, registers without PC
+     * backup Rtemp, set to PC
+     * if W=0: ...
+     * if W=1: ...
+     * restore Rtemp
+     *
+     * STMIB similar to STMIA but starts at Rn+4
+     * perform STMIB Rn, registers without PC
+     * backup Rtemp, set to PC
+     * if W=0: STR offset
+     * if W=1: STR PRE-indexed
+     * restore Rtemp
+     */
+    DIE_NOW(NULL, "STM.. Rn, ..-PC not implemented");
   }
+
+  currBlockCopyCacheAddr = updateCodeCachePointer(tc, currBlockCopyCacheAddr);
+  *currBlockCopyCacheAddr = instruction;
+  return ++currBlockCopyCacheAddr;
 }
