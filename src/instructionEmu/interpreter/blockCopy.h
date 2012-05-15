4d77752877787bbb670464803961b73cb492c25c
Niels Penneman <niels.penneman@elis.ugent.be>
2012-05-15 12:02:32 +0200

    Simplify getOtherRegisterOfN

--- src/instructionEmu/interpreter/blockCopy.h
+++ src/instructionEmu/interpreter/blockCopy.h
@@ -23,12 +23,12 @@ __macro__ u32int getOtherRegisterOf3(u32int usedRegister1, u32int usedRegister2,
  * obviously, always returns 0, 1 or 2 for any 2 registers */
 __macro__ u32int getOtherRegisterOf2(u32int usedRegister1, u32int usedRegister2)
 {
-  return countTrailingZeros(0xFFFF & ~((1 << usedRegister1) | (1 << usedRegister2)));
+  return countTrailingZeros(~((1 << usedRegister1) | (1 << usedRegister2)));
 }
 /* returns 0,1,2,3 for any 3... DUH */
 __macro__ u32int getOtherRegisterOf3(u32int usedRegister1, u32int usedRegister2, u32int usedRegister3)
 {
-  return countTrailingZeros(0xFFFF & ~((1 << usedRegister1) | (1 << usedRegister2) | (1 << usedRegister3)));
+  return countTrailingZeros(~((1 << usedRegister1) | (1 << usedRegister2) | (1 << usedRegister3)));
 }
 
 #endif /* __INSTRUCTION_EMU__INTERPRETER__BLOCK_COPY_H__ */
