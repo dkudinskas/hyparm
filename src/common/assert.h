2a41bceb187d98e74e34271c0fc8ae16b228b5d9
Niels Penneman <niels.penneman@elis.ugent.be>
2012-05-18 14:37:37 +0200

    Fix comments in common/assert.h

--- src/common/assert.h
+++ src/common/assert.h
@@ -8,6 +8,6 @@
 #define COMPILE_TIME_ASSERT(exp, name)  _Static_assert(exp, #name)
 #else
 #define COMPILE_TIME_ASSERT(exp, name)  typedef char assertion_failed##name [ (exp ) ? 1 : -1 ]
-#endif
-
 #endif /* COMPILER_HAS_STATIC_ASSERT */
+
+#endif /* __COMMON__ASSERT_H__ */
