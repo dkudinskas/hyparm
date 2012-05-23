c6feca74a8424f4273e563712cc64c7c2de523c2
Niels Penneman <niels.penneman@elis.ugent.be>
2012-05-23 23:08:24 +0200

    Unit tests: fill guest memory with fixed pattern before boot

--- src/guestBoot/test.c
+++ src/guestBoot/test.c
@@ -1,9 +1,13 @@
 #include "common/debug.h"
 #include "common/byteOrder.h"
+#include "common/linker.h"
 #include "common/string.h"
 
 #include "guestBoot/test.h"
 #include "guestBoot/loader.h"
+
+#include "memoryManager/memoryConstants.h"
+
 
 struct testImageHeader getTestImageHeader(ulong uImageAddr)
 {
@@ -48,6 +52,17 @@ void bootTest(GCONTXT *context, u32int imageAddress)
         loadAddress);
     memmove((void *)loadAddress, (const void *)currentAddress, sizeInBytes);
   }
+
+  /*
+   * Fill guest memory with fixed pattern to ensure no data is left from previous runs. Even after
+   * a 'hard' reset, the power to the SDRAM is never cut so its contents remain.
+   */
+  const u32int endAddress = loadAddress + sizeInBytes;
+  const u32int pattern = 0x88442211;
+  DEBUG(STARTUP, "Filling guest memory with pattern %#.8x...", pattern);
+  memset((void *)MEMORY_START_ADDR, pattern, loadAddress - MEMORY_START_ADDR);
+  memset((void *)endAddress, pattern, HYPERVISOR_BEGIN_ADDRESS - endAddress);
+  DEBUG(STARTUP, " done" EOL);
 
   bootGuest(context, GUEST_OS_TEST, entryPoint);
 }
