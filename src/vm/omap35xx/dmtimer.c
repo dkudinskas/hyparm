60b08e9d43a4eeb39301799ce9d16f6d431055a2
Henri De Veene <henri.deveene@ugent.be>
2012-05-24 21:40:22 +0200

    Added comment to DM Timer.

--- src/vm/omap35xx/dmtimer.c
+++ src/vm/omap35xx/dmtimer.c
@@ -10,6 +10,11 @@
  * tries to initialize this device.
  * See: arch/arm/plat-omap/dmtimer.c
  *
+ * Note 1: It seems this is the General-Purpose Timer 12 described in the
+ *         OMAP35x manual spruf98d.pdf. GPT12 was later removed from the manual.
+ *
+ * Note 2: GP Timers are renamed to DM (Dual-Mode) Timers in OMAP4
+ *
  * There's an option in the kernel config but disabling it breaks the build.
  */
 
