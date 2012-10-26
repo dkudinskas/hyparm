195cf3ca1ef38cb0ca70c09b928e2335bd2173b2
Danielius Kudinskas <danieliaus@yahoo.com>
2012-10-26 18:44:18 +0100

    more counters build

--- src/exceptions/exceptionHandlers.h
+++ src/exceptions/exceptionHandlers.h
@@ -8,48 +8,6 @@
 #include "memoryManager/mmu.h"
 
 
-#ifdef CONFIG_EXCEPTION_HANDLERS_COUNT_DATA_ABORT
-
-u64int getDataAbortCounter(void);
-
-void resetDataAbortCounter(void);
-
-#else
-
-#define getDataAbortCounter()    (0ULL)
-#define resetDataAbortCounter()
-
-#endif /* CONFIG_EXCEPTION_HANDLERS_COUNT_DATA_ABORT */
-
-
-#ifdef CONFIG_EXCEPTION_HANDLERS_COUNT_IRQ
-
-u64int getIrqCounter(void);
-
-void resetIrqCounter(void);
-
-#else
-
-#define getIrqCounter()    (0ULL)
-#define resetIrqCounter()
-
-#endif /* CONFIG_EXCEPTION_HANDLERS_COUNT_IRQ */
-
-
-#ifdef CONFIG_EXCEPTION_HANDLERS_COUNT_SVC
-
-u64int getSvcCounter(void);
-
-void resetSvcCounter(void);
-
-#else
-
-#define getSvcCounter()    (0ULL)
-#define resetSvcCounter()
-
-#endif /* CONFIG_EXCEPTION_HANDLERS_COUNT_SVC */
-
-
 GCONTXT *softwareInterrupt(GCONTXT *context, u32int code);
 
 GCONTXT *dataAbort(GCONTXT *context);
