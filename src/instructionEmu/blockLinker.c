0758823f4982a61e7181739d9b8f82951654faf5
Danielius Kudinskas <danielius.kudinskas@gmail.com>
2012-11-28 20:06:07 +0200

    forgot 2 files

---
+++ src/instructionEmu/blockLinker.c
@@ -0,0 +1,113 @@
+#include "common/debug.h"
+#include "cpuArch/constants.h"
+
+#include "instructionEmu/blockLinker.h"
+#include "instructionEmu/decoder/arm/structs.h"
+#include "instructionEmu/scanner.h"
+#include "instructionEmu/translator/translator.h"
+
+#include "memoryManager/mmu.h"
+
+
+void linkBlock(GCONTXT *context, u32int nextPC, u32int lastPC, BasicBlock* lastBlock)
+{
+  // get next block
+  u32int blockIndex = getBasicBlockStoreIndex(nextPC);
+  BasicBlock* nextBlock = getBasicBlockStoreEntry(context->translationStore, blockIndex);
+  
+  // if invalid, return.
+  if ((nextBlock->type == BB_TYPE_INVALID) || (nextBlock->hotness < 10))
+  {
+    // next block not scanned.
+    return;
+  }
+
+  u32int addressMap = context->virtAddrEnabled ? (u32int)context->pageTables->guestPhysical : 0;
+  if (((u32int)nextBlock->guestStart != nextPC) || (nextBlock->addressMap != addressMap))
+  {
+    // conflict. return, let scanBlock() deal with conflict first, link later.
+    return;
+  }
+
+  u32int eobInstruction = *(lastBlock->guestEnd);
+  if (!isBranch(eobInstruction))
+  {
+    // not a branch. just leave hypercall.
+    return;
+  }
+
+
+  // if we found a group block with an old version number, fail to link. 
+  // scanner will then re-scan this block, thus validating it's correct
+  if ((nextBlock->type == GB_TYPE_ARM) && (nextBlock->versionNumber != context->groupBlockVersion))
+  {
+    return;
+  }
+  if ((lastBlock->type == GB_TYPE_ARM) && (lastBlock->versionNumber != context->groupBlockVersion))
+  {
+    return;
+  }
+
+  // must check wether it was the first or second hypercall
+  u32int lastInstrOfHostBlock = (u32int)lastBlock->codeStoreStart +
+                                (lastBlock->codeStoreSize-1) * ARM_INSTRUCTION_SIZE;
+  lastInstrOfHostBlock -= ARM_INSTRUCTION_SIZE;
+  u32int conditionCode = 0xE0000000;
+  if (lastPC != lastInstrOfHostBlock)
+  {
+    conditionCode = eobInstruction & 0xF0000000;
+  }
+
+  // must try to put a branch at lastPC direct to nextPC
+  putBranch(lastPC, (u32int)nextBlock->codeStoreStart, conditionCode);
+
+  // make sure both blocks are marked as a group-block.
+  lastBlock->type = GB_TYPE_ARM;
+  lastBlock->versionNumber = context->groupBlockVersion;
+  nextBlock->type = GB_TYPE_ARM;
+  nextBlock->versionNumber = context->groupBlockVersion;
+}
+
+
+void unlinkBlock(BasicBlock* block, u32int index)
+{
+  u32int hypercall = INSTR_SWI | (index + 0x100);
+  u32int lastInstrOfHostBlock = (u32int)block->codeStoreStart +
+                                (block->codeStoreSize-1) * ARM_INSTRUCTION_SIZE;
+  lastInstrOfHostBlock -= ARM_INSTRUCTION_SIZE;
+
+  // remove 'last' hypercall (or the only one if there werent more)
+  *(u32int*)lastInstrOfHostBlock = hypercall;
+  mmuCleanDCacheByMVAtoPOU(lastInstrOfHostBlock);
+  mmuInvIcacheByMVAtoPOU(lastInstrOfHostBlock);
+
+  if (!block->oneHypercall)
+  {
+    // two hypercalls! fix up condition code.
+    u32int condition = *(u32int*)(lastInstrOfHostBlock-ARM_INSTRUCTION_SIZE);
+    condition &= 0xF0000000; 
+    hypercall = (hypercall & 0x0FFFFFFF) | condition;
+    
+    *(u32int*)(lastInstrOfHostBlock-ARM_INSTRUCTION_SIZE) = hypercall;
+    mmuCleanDCacheByMVAtoPOU(lastInstrOfHostBlock-ARM_INSTRUCTION_SIZE);
+    mmuInvIcacheByMVAtoPOU(lastInstrOfHostBlock-ARM_INSTRUCTION_SIZE);
+  }
+  block->type = BB_TYPE_ARM;
+  block->versionNumber = 0;
+}
+
+
+void unlinkAllBlocks(GCONTXT *context)
+{
+  DIE_NOW(0, "unlinkAllBlocks unimplemented.\n");
+  u32int i = 0;
+  /* we traverse the complete block translation store
+   * inside the loop we unlink all current group-blocks. */ 
+  for (i = 0; i < BASIC_BLOCK_STORE_SIZE; i++)
+  {
+    if (context->translationStore->basicBlockStore[i].type == GB_TYPE_ARM)
+    {
+      unlinkBlock(&context->translationStore->basicBlockStore[i], i);
+    }
+  }
+}
---
+++ src/instructionEmu/blockLinker.h
@@ -0,0 +1,13 @@
+#ifndef __INSTRUCTION_EMU__BLOCK_LINKER_H__
+#define __INSTRUCTION_EMU__BLOCK_LINKER_H__
+
+#include "common/types.h"
+
+#include "guestManager/basicBlockStore.h"
+#include "guestManager/guestContext.h"
+
+void linkBlock(GCONTXT *context, u32int nextPC, u32int lastPC, BasicBlock* lastBlock); 
+void unlinkBlock(BasicBlock* block, u32int index);
+void unlinkAllBlocks(GCONTXT *context);
+
+#endif
